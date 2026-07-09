25452b49adc9525fa70a1ec8acfcc937b8ac2715
xheisi <xheisihida483@gmail.com>
2026-07-09 16:48:35 +0200

    Refactor GreetingService to use GreetingProperties
    Comment out the greeting field and update constructor to use GreetingProperties.

--- springBoot/main/java/com/example/SpringBootLufthansa/GreetingService.java
+++ springBoot/main/java/com/example/SpringBootLufthansa/GreetingService.java
@@ -5,10 +5,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+    //java -jar SpringBootLufthansa-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev/prod
 
-    @Value("${app.greeting:Hii}")
-    private String greeting;
 
+    //@Value("${app.greeting:Hello}")
+    //private String greeting;
+    private final GreetingProperties greetingProperties;
+
+    public GreetingService(GreetingProperties greetingProperties) {
+        this.greetingProperties = greetingProperties;
+    }
     public String greet(String name) {
         return greeting + " " + name;
     }
