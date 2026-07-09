e165f8f2a6f9cec6f34004e252598cf50a3a972c
xheisi <xheisihida483@gmail.com>
2026-07-09 16:47:49 +0200

    Enable configuration properties for GreetingProperties

--- springBoot/main/java/com/example/SpringBootLufthansa/SpringBootLufthansaApplication.java
+++ springBoot/main/java/com/example/SpringBootLufthansa/SpringBootLufthansaApplication.java
@@ -2,12 +2,14 @@ package com.example.SpringBootLufthansa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GreetingProperties.class)
 public class SpringBootLufthansaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLufthansaApplication.class, args);
 	}
-
+		//RUN: mvn spring-boot:run
 }
